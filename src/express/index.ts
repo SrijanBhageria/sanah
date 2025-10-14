1326da117571a6713e0d5ec0ba1a8f1f056d8d0f
KanwarAdityaSingh <kanwaradityasingh30@gmail.com>
2025-10-15 04:23:07 +0530

    Fix CORS, helmet, and morgan imports for CommonJS compatibility

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,8 +1,8 @@
 import * as express from 'express';
 import { Application } from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import * as cors from 'cors';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
 import { env } from '../config/env';
 import { logger } from '../logger/logger';
 import { errorHandler, notFoundHandler } from '../middleware/error.middleware';
